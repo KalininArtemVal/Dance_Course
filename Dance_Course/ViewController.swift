feda7befd7082e082fd1c86a9ef010718b3a171f
Kalinin Artem <KalininArtemVal@gmail.com>
2022-02-08 20:45:06 +0300

    Initial Commit

---
+++ Dance_Course/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dance_Course
+//
+//  Created by Артем Калинин on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
